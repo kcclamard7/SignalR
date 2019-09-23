dd5da1b732340fb8f3c57938da1f930100165833
Christ Kaya <christclamard@outlook.com>
2019-09-23 12:08:27 -0500

    firstcommit

---
+++ SignalRTest/Controllers/DemoController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalRTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRTest.Controllers
+{
+
+    [Route("api/[controller]")]
+    public class DemoController : Controller
+    {
+        public IHubContext<ChatHub> Myhub { get; }
+        public DemoController(IHubContext<ChatHub> myhub)
+        {
+            Myhub = myhub;
+        }
+
+       
+
+        [Route("getlist")]
+        [HttpGet]
+        public async Task<IActionResult>  Getlist()
+        {
+        
+          
+            Dictionary<string, string> Listname = new Dictionary<string, string>()
+            {
+                ["toto"] = "my mom",        
+                ["nono"] = "my friend",
+                ["titi"] = "my uncle",
+                ["lolo"] = "my nephew",
+            };
+
+            foreach(var i in Listname)
+            {
+
+                 await Myhub.Clients.All.SendAsync("ReceiveMessage", i.Key, i.Value);     
+                 
+            }
+            return Ok("done");
+        }
+       
+    }
+}
---
+++ SignalRTest/Startup.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SignalRTest.Models;
+
+namespace SignalRTest
+{
+    public class Startup
+    {
+        public readonly IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+                services.AddCors(options =>
+                {
+                    options.AddPolicy("CorsPolicy",
+                                 builder =>
+                                 {
+                                     builder.AllowAnyMethod()
+                                            .AllowAnyHeader()
+                                            .AllowCredentials()
+                                            .WithOrigins("https://localhost:44326");
+
+                                 });
+                }
+            );
+
+            services.AddSignalR();
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseHsts();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                //app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseCors("CorsPolicy");
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<ChatHub>("/chatHub");
+            });
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
